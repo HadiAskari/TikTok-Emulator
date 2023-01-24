2132cb432be2d3f1c9df500049aa3eea2c72de12
Muhammad Haroon <m_haroon96@hotmail.com>
2023-01-23 16:30:30 -0800

    finished singular flow

--- main.py
+++ main.py
@@ -52,26 +52,45 @@ def swipe_up(device):
     # swipe to next video
     device.swipe((200, 500), (200, 0))
 
-def audit(device):
-    # swipe over video
-    for i in range(100):
-        xml = device.get_xml()
-        
-        # play/pause video
-        device.tap((200, 200))
+def play_pause(device):
+    # pause video
+    device.tap((300, 300))
 
-
-#device = emulate_new_device()
+# get physical device
 device = get_connected_devices()[0]
+
+# prep tiktok payload
 init_device(device)
-login(device, 'auditshorts1@gmail.com', 'ytshortstiktok123!')
 
+# pause video
+play_pause(device)
 
-#def main():
-#    print("VNC link:", device.get_vnc_link())
-#    try:
-#    finally:
-#        device.shutdown()
+# click on search button
+elem = device.find_element(attrs={'resource-id': 'com.ss.android.ugc.trill:id/g1p'})
+elem = elem.find_all(attrs={'class': 'android.widget.ImageView'})[-1]
+coords = device.get_coordinates(elem)
+device.tap(coords)
 
-#if __name__ == '__main__':
-#    main()
+# enter search query
+device.type_text(18)
+device.type_text('abortion')
+
+# click search button
+elem = device.find_element(attrs={'text': 'Search'})
+coords = device.get_coordinates(elem)
+device.tap(coords)
+
+# click first video
+elem = device.find_element(attrs={'resource-id': 'com.ss.android.ugc.trill:id/x1'})
+coords = device.get_coordinates(elem)
+device.tap(coords)
+
+# swipe through videos
+for i in range(10):
+    # watch for 30s
+    sleep(30)
+    # pause video to grab xml
+    play_pause(device)
+    xml = device.get_xml()
+    # do something here
+    swipe_up(device)
