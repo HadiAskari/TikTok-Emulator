a46ed8f9790f7aa020d949cace7cda35065ff3c2
Muhammad Haroon <m_haroon96@hotmail.com>
2023-01-30 16:37:44 -0800

    changes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def init_device(device):
     #device.install_apk('/home/hadi/Desktop/TikTok-Emulator/tiktok.apk')
 
     # launch tiktok app
+    device.kill_app('com.ss.android.ugc.trill')
     device.launch_app('com.ss.android.ugc.trill')
 
     # wait for app to load
@@ -63,95 +64,99 @@ def play_pause(device):
 #device = emulate_new_device()
 device = get_connected_devices()[0]
 
-# prep tiktok payload
-init_device(device)
+# # prep tiktok payload
+# init_device(device)
 
-# click on search button
-device.tap((1000, 120))
+# # click on search button
+# device.tap((1000, 120))
 
-# # enter search query
-device.type_text(18)
-device.type_text('cricket')
+# # # enter search query
+# device.type_text(18)
+# device.type_text('cricket')
 
-# # click search button
-elem = device.find_element(attrs={'text': 'Search'})
-coords = device.get_coordinates(elem)
-device.tap(coords)
-
-# click first video
-# elem = device.find_element(attrs={'resource-id': 'com.ss.android.ugc.trill:id/x1'})
+# # # click search button
+# elem = device.find_element(attrs={'text': 'Search'})
 # coords = device.get_coordinates(elem)
 # device.tap(coords)
 
-#problematic tap
-# elem = device.find_element(attrs={'text': 'lakhan.singh007'})
-# coords = device.get_coordinates(elem)
-# print(coords)
-sleep(5)
-device.tap((250, 750))
+# # click first video
+# # elem = device.find_element(attrs={'resource-id': 'com.ss.android.ugc.trill:id/x1'})
+# # coords = device.get_coordinates(elem)
+# # device.tap(coords)
 
-Traindata=defaultdict(list)
+# # problematic tap
+# # elem = device.find_element(attrs={'text': 'lakhan.singh007'})
+# # coords = device.get_coordinates(elem)
+# # print(coords)
+# sleep(5)
+# device.tap((250, 750))
 
-desc_train=[]
-username_train=[]
+# Traindata=defaultdict(list)
+
+# desc_train=[]
+# username_train=[]
 
 
-#Training
-# swipe through videos
-for i in range(100):
-    # watch for 30s
-    sleep(30)
-    # pause video to grab xml
-    # print('here')
-    play_pause(device)
+# #Training
+# # swipe through videos
+# for i in range(100):
+#     print(i)
+#     # watch for 30s
+#     sleep(30)
+#     # pause video to grab xml
+#     # print('here')
+#     play_pause(device)
+
+#     sleep(5)
     
-    # check if see more exists
-    elem = device.find_element(attrs={'text': 'See more'})
-    if elem is not None:
-        coords = device.get_coordinates(elem)
-        device.tap(coords)
+#     # check if see more exists
+#     elem = device.find_element(attrs={'text': 'See more'})
+#     if elem is not None:
+#         coords = device.get_coordinates(elem)
+#         device.tap(coords)
 
+#     elem = device.find_element(attrs={'content-desc': 'Like'})
+#     if elem is not None:
+#         print('liking')
+#         coords = device.get_coordinates(elem)
+#         device.tap(coords)
 
-    # grab description here and reverse-google search
-    xml = device.get_xml_file()
-    tree=ET.parse(xml)
+#     # grab description here and reverse-google search
+#     try:
+#         xml = device.get_xml_file()
+#         tree=ET.parse(xml)
+        
+#         for elem in tree.iter():
+#             dic=elem.attrib
+
+            # if dic.get('text', '') != '':
+                # row[dic['resource-id']] = dic['text']
+#     except:
+#         play_pause(device)
+#         continue
     
-    for elem in tree.iter():
-        dic=elem.attrib
-        try:
-            if dic['resource-id']=="com.ss.android.ugc.trill:id/bc5":
-                desc_train.append(dic['text'])
-        except:
-            continue
+#     # move to next video
+#     swipe_up(device)
 
-        try:
-            if dic['resource-id']=="com.ss.android.ugc.trill:id/title":
-                username_train.append(dic['text'])
-        except:
-            continue
-    
-    # move to next video
-    swipe_up(device)
+# print(desc_train)
+# print(username_train)
 
-print(desc_train)
-print(username_train)
-
-train_dict={'Usernames':username_train, 'Descriptions': desc_train}
-df_train=pd.DataFrame.from_dict(train_dict)
-df_train.to_csv("Training.csv")
+# train_dict={'Usernames':username_train, 'Descriptions': desc_train}
+# df_train=pd.DataFrame.from_dict(train_dict)
+# df_train.to_csv("Training.csv")
 
 
-device.kill_app('com.ss.android.ugc.trill')
-device.launch_app('com.ss.android.ugc.trill')
+# device.kill_app('com.ss.android.ugc.trill')
+# device.launch_app('com.ss.android.ugc.trill')
 
 #Test
 
 Testdata=defaultdict(list)
 
-desc_test=[]
-username_test=[]
+rows = []
 
 for i in range(50):
+    print(i)
     sleep(1)
     # pause video to grab xml
     # print('here')
@@ -165,26 +170,25 @@ for i in range(50):
 
 
     # grab description here and reverse-google search
-    xml = device.get_xml_file()
-    tree=ET.parse(xml)
+    try:
+        xml = device.get_xml_file()
+        tree=ET.parse(xml)
+        row = {}
 
-    for elem in tree.iter():
-        dic=elem.attrib
-        try:
-            if dic['resource-id']=="com.ss.android.ugc.trill:id/bc5":
-                desc_test.append(dic['text'])
-        except:
-            continue
+        for elem in tree.iter():
+            dic=elem.attrib
+            if dic.get('text', '') != '':
+                row[dic['resource-id']] = dic['text']
 
-        try:
-            if dic['resource-id']=="com.ss.android.ugc.trill:id/title":
-                username_test.append(dic['text'])
-        except:
-            continue
+        print(row)
+        rows.append(row)
+    except Exception as e:
+        print(e)
+        play_pause(device)
+        continue
     
     # move to next video
     swipe_up(device)
 
-test_dict={'Usernames':username_test, 'Descriptions': desc_test}
-df_test=pd.DataFrame.from_dict(test_dict)
-df_test.to_csv("Testing.csv")
+df_test=pd.DataFrame(rows)
+df_test.to_csv("Testing-Haroon.csv")
