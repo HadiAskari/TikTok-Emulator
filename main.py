d5f95f24b485f155f97df162f96b71e002770516
Muhammad Haroon <m_haroon96@hotmail.com>
2023-02-15 16:42:05 -0800

    moved to utils

--- main.py
+++ main.py
@@ -11,37 +11,6 @@ import json
 import re
 from transformers import pipeline
 
-
-def remove_emojis(data):
-    emoj = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        u"\U00002500-\U00002BEF"  # chinese char
-        u"\U00002702-\U000027B0"
-        u"\U00002702-\U000027B0"
-        u"\U000024C2-\U0001F251"
-        u"\U0001f926-\U0001f937"
-        u"\U00010000-\U0010ffff"
-        u"\u2640-\u2642" 
-        u"\u2600-\u2B55"
-        u"\u200d"
-        u"\u23cf"
-        u"\u23e9"
-        u"\u231a"
-        u"\ufe0f"  # dingbats
-        u"\u3030"
-                    "]+", re.UNICODE)
-    return re.sub(emoj, '', data)
-
-def preprocess(text):
-
-    text=' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",text).split())
-    text = text.lower()
-    return text  
-
-
 def parse_args():
     args = ArgumentParser()
     args.add_argument('--query', required=True)
@@ -215,10 +184,10 @@ def test(device, query):
             # like video if it contains the query needed
             if el['resource-id']=='com.ss.android.ugc.trill:id/bc5':
                 try:
-                    text=remove_emojis(el['text'])
+                    text = util.remove_emojis(el['text'])
                 except:
                     text="Unrecognized"
-                text=preprocess(text)
+                text = util.preprocess(text)
                 if text== "":
                     text="Empty"
 
--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import names
 from random import choice, randint
 from uuid import uuid4
+import re
 
 def generate_email():
     domains = ['hotmail.com', 'gmail.com', 'yahoo.com', 'live.com']
@@ -25,3 +26,32 @@ def tap_on(device, attrs):
 
 def lower_keyboard(device):
     device.type_text(111)
+
+
+def remove_emojis(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                    "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
+
+def preprocess(text):
+    text = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",text).split())
+    text = text.lower()
+    return text  
